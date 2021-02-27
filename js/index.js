063a6fb196142771051a7fd3a5dfdd2c2a7d26b9
Daniel Vaini <dnlvaini@gmail.com>
2021-02-27 13:52:41 -0300

    feat: Adicionando evento ao background-menu

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 let input = document.getElementById('bt-menu')
 let menu = document.querySelector('.menu')
+let menuBackground = document.querySelector('.background-menu-mobile');
 input.addEventListener('click', function(){
   if(input.checked){
-    menu.style.left = '12.5%'
+    menu.style.left = '12.5%';
+    menuBackground.style.left = '0px';
+   
   }else {
     
     menu.style.left = '-102.5%'
+    menuBackground.style.left = '-102.5%';
+   
   }
 })
