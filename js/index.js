2f980c28091449763038121353fc3784d997d27a
Daniel Vaini <dnlvaini@gmail.com>
2021-03-02 19:13:28 -0300

    feat: Formatando codigo

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-let input = document.getElementById('bt-menu')
-let menu = document.querySelector('.menu')
-let menuBackground = document.querySelector('.background-menu-mobile');
+const input = document.getElementById('bt-menu')
+const menu = document.querySelector('.menu')
+const menuBackground = document.querySelector('.background-menu-mobile');
+
 input.addEventListener('click', function(){
   if(input.checked){
     menu.style.left = '12.5%';
     menuBackground.style.left = '0px';
+  
     
   }else {
     
