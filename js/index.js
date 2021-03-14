d442fe0306ebe8ab04aec8eca8194ce8ced604a4
Daniel Vaini <dnlvaini@gmail.com>
2021-03-14 18:09:56 -0300

    feat: Adding a event for new input radio created

--- js/index.js
+++ js/index.js
@@ -22,22 +22,34 @@ button.addEventListener('click', () => {
 // modal
 const inputRadio = document.querySelectorAll('#option')
 const divNonation = document.querySelectorAll('.entrie-donation')
-const divBamboo = document.querySelector('.bamboo-stang-card')
+const cards = document.querySelectorAll('.card')
 const hr = document.querySelectorAll('hr')
 
 inputRadio.forEach((item) => {
   item.addEventListener('click',() => {
     if(inputRadio[1].checked){
-      console.log(inputRadio)
+
       divNonation[0].style.display = 'flex';
       divNonation[0].style.top = '0px'; 
-      divBamboo.style.height = 250+'px';
-      hr.style.display = 'block'
-    }else {
+      cards[0].style.height = 250+'px';
+     
+    }else if (!inputRadio[1].checked){
       divNonation[0].style.display = 'none';
-      divBamboo.style.height = 136+'px';
-      hr.style.display = 'none'
+      cards[0].style.height = 136+'px';
+     
 
+    }
+    if(inputRadio[2].checked){
+     console.log('Checked')
+     divNonation[1].style.display = 'flex';
+     divNonation[1].style.top = '0px'; 
+     cards[1].style.height = 250+'px';
+    
+    }else if (!inputRadio[2].checked){
+      console.log('No Checked')
+      divNonation[1].style.display = 'none';
+      cards[1].style.height = 136+'px';
+     
     }
 
 
